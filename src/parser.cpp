98a9cf8c6fb8101b0c22f732dd357bba67265c06
student <miguelmancera997@yahoo.com>
2022-10-26 10:19:15 -0700

    Sombra is the best

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,13 +1,13 @@
 /**
  *  CPSC 323 Compilers and Languages
- * 
+ *
  *  Dalton Caron, Teaching Associate
  *  dcaron@fullerton.edu, +1 949-616-2699
  *  Department of Computer Science
  */
 #include <parser.h>
 
-Parser::Parser(const token_stream_t &tokens) 
+Parser::Parser(const token_stream_t &tokens)
 : tokens(tokens) {}
 
 Parser::~Parser() {}
@@ -17,12 +17,12 @@ ParseTree<std::string> Parser::parse() {
 }
 
 token_t Parser::getNextToken() {
-    // Assumes that the end of the token stream always has 
+    // Assumes that the end of the token stream always has
     // the end of file enumeration.
     if (this->current_token == this->tokens.size()) {
         return this->tokens[this->current_token - 1];
     }
-    
+
     return this->tokens[this->current_token++];
 }
 
@@ -35,7 +35,7 @@ token_t Parser::peekNextToken() const {
 
 // This is the example that was provided in the document.
 PTPtr<std::string> Parser::parseProgram() {
-    PTPtr<std::string> programNode = 
+    PTPtr<std::string> programNode =
         std::make_shared<PTNode<std::string>>("program");
     programNode->addChild(this->parseBlock());
     this->tryMatchTerminal(this->getNextToken(), PERIOD, programNode);
@@ -43,111 +43,113 @@ PTPtr<std::string> Parser::parseProgram() {
 }
 
 PTPtr<std::string> Parser::parseBlock() {
-    PTPtr<std::string> blockNode = 
+    PTPtr<std::string> blockNode =
         std::make_shared<PTNode<std::string>>("block");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+    blockNode->addChild(this->parseConstDeclarations());
+    blockNode->addChild(this->parseVarDeclarations());
+    blockNode->addChild(this->parseProcedure());
+    blockNode->addChild(this->parseStatement());
+
+
+
+    //this->tryMatchTerminal(this->getNextToken(), {RIGHT_PAREN, NUMBER_LITERAL, IDENTIFIER, END_KEYWORD}, blockNode);
+
     return blockNode;
 }
 
 PTPtr<std::string> Parser::parseConstDeclarations() {
-    PTPtr<std::string> constDeclNode = 
+    PTPtr<std::string> constDeclNode =
         std::make_shared<PTNode<std::string>>("const_declaration");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+
+      constDeclNode->addChild(this->parseConstDeclarationList());
+
+      /*if (constDeclNode == CONST_KEYWORD) {
+        // ACTIONS
+      }*/
+
+      this->tryMatchTerminal(this->getNextToken(), SEMICOLON, constDeclNode);
     return constDeclNode;
 }
 
 PTPtr<std::string> Parser::parseConstDeclarationList() {
-    PTPtr<std::string> constDeclListNode = 
+    PTPtr<std::string> constDeclListNode =
         std::make_shared<PTNode<std::string>>("const_declaration_list");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+
+        constDeclListNode->addChild(this->parseVarDeclarations());
+
+        this->tryMatchTerminal(this->getNextToken(), NUMBER_LITERAL, constDeclListNode);
     return constDeclListNode;
 }
 
 PTPtr<std::string> Parser::parseVarDeclarations() {
-    PTPtr<std::string> varDeclNode = 
+    PTPtr<std::string> varDeclNode =
         std::make_shared<PTNode<std::string>>("var_declaration");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+        varDeclNode->addChild(this->parseVarDeclarationList());
+
+        /*
+        if (varDeclNode == VAR_KEYWORD) {
+          ACTIONS
+        }
+        */
+
+
+
+        this->tryMatchTerminal(this->getNextToken(), {RIGHT_PAREN, NUMBER_LITERAL, IDENTIFIER}, varDeclNode);
     return varDeclNode;
 }
 
 PTPtr<std::string> Parser::parseVarDeclarationList() {
-    PTPtr<std::string> varDeclNodeList = 
+    PTPtr<std::string> varDeclNodeList =
         std::make_shared<PTNode<std::string>>("var_declaration_list");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+        varDeclNodeList->addChild(this->parseProcedure());
+        this->tryMatchTerminal(this->getNextToken(), {RIGHT_PAREN, NUMBER_LITERAL, IDENTIFIER}, varDeclNodeList);
     return varDeclNodeList;
 }
 
 PTPtr<std::string> Parser::parseProcedure() {
-    PTPtr<std::string> procedure = 
+    PTPtr<std::string> procedure =
         std::make_shared<PTNode<std::string>>("procedure");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+        procedure->addChild(this->parseStatement());
+        this->tryMatchTerminal(this->getNextToken(), SEMICOLON, procedure);
     return procedure;
 }
 
 PTPtr<std::string> Parser::parseStatement() {
-    PTPtr<std::string> statementNode = 
+    PTPtr<std::string> statementNode =
         std::make_shared<PTNode<std::string>>("statement");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+        statementNode->addChild(this->parseCondition());
+        this->tryMatchTerminal(this->getNextToken(), {RIGHT_PAREN, NUMBER_LITERAL, IDENTIFIER, END_KEYWORD}, statementNode);
     return statementNode;
 }
 
 PTPtr<std::string> Parser::parseCondition() {
-    PTPtr<std::string> conditionNode = 
+    PTPtr<std::string> conditionNode =
         std::make_shared<PTNode<std::string>>("condition");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+     conditionNode->addChild(this->parseExpression());
+     this->tryMatchTerminal(this->getNextToken(), {RIGHT_PAREN, NUMBER_LITERAL, IDENTIFIER}, conditionNode);
     return conditionNode;
 }
 
 PTPtr<std::string> Parser::parseExpression() {
-    PTPtr<std::string> expressionNode = 
+    PTPtr<std::string> expressionNode =
         std::make_shared<PTNode<std::string>>("expression");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+        expressionNode->addChild(this->parseTerm());
+        this->tryMatchTerminal(this->getNextToken(), {RIGHT_PAREN, NUMBER_LITERAL, IDENTIFIER}, expressionNode);
     return expressionNode;
 }
 
 PTPtr<std::string> Parser::parseTerm() {
     PTPtr<std::string> termNode = std::make_shared<PTNode<std::string>>("term");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+    termNode->addChild(this->parseFactor());
+    this->tryMatchTerminal(this->getNextToken(), {RIGHT_PAREN, NUMBER_LITERAL, IDENTIFIER}, termNode);
     return termNode;
 }
 
 PTPtr<std::string> Parser::parseFactor() {
-    PTPtr<std::string> factorNode = 
+    PTPtr<std::string> factorNode =
         std::make_shared<PTNode<std::string>>("factor");
-    /**
-     * Your implementation here.
-     */
-    WARNING_LOG("%s: %s has not been implemented yet!", __FILE__, __func__);
+
+        this->tryMatchTerminal(this->getNextToken(), {RIGHT_PAREN, NUMBER_LITERAL, IDENTIFIER}, factorNode);
     return factorNode;
 }
 
@@ -163,7 +165,7 @@ void Parser::tryMatchTerminal(
 }
 
 void Parser::tryMatchTerminal(
-    const token_t &actual, 
+    const token_t &actual,
     const std::initializer_list<token_class_t> expected,
     PTPtr<std::string> node
 ) const {
@@ -177,7 +179,7 @@ void Parser::tryMatchTerminal(
 }
 
 void Parser::raiseMismatchError(
-        const token_t &actual, 
+        const token_t &actual,
         const token_class_t expected
 ) const {
     ERROR_LOG(
@@ -194,7 +196,7 @@ void Parser::raiseMismatchError(
 }
 
 void Parser::raiseMismatchError(
-        const token_t &actual, 
+        const token_t &actual,
         const std::initializer_list<token_class_t> expected
 ) const {
     ERROR_LOG("expected ");
@@ -207,7 +209,7 @@ void Parser::raiseMismatchError(
     }
 
     ERROR_LOG(
-        "got %s at %s line %d column %d", 
+        "got %s at %s line %d column %d",
         actual.lexeme.c_str(),
         actual.file.c_str(),
         actual.line,
